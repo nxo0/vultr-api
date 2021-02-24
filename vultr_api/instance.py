42ef41688f757695980ef72c54eeb8077eb9eee8
nxo0 <ninomiyakan20051011@gmail.com>
2021-02-24 21:15:59 +0900

    🌞 update

--- vultr_api/instance.py
+++ vultr_api/instance.py
@@ -5,31 +5,40 @@ import json
 
 
 def vm_list(api_key):
-    if type(api_key) is str:
-        req = urllib.request.Request('https://api.vultr.com/v2/instances')
-        req.add_header('Authorization', f'Bearer {api_key}')
-        with urllib.request.urlopen(req) as res:
-            res = res.read().decode('utf-8')
-            return res
-    else:
-        logger.err("[vultr-api] APIキーはString型で指定してください。")
+    """List of VMs that are deployed.
+
+    """
+    try:
+        if type(api_key) is str:
+            req = urllib.request.Request('https://api.vultr.com/v2/instances')
+            req.add_header('Authorization', f'Bearer {api_key}')
+            with urllib.request.urlopen(req) as res:
+                res = res.read().decode('utf-8')
+                return res
+        else:
+            logger.err("[vultr-api] APIキーはString型で指定してください。")
+    except Exception as e:
+        logger.err(e)
 
 
 def vm_create(api_key, region, plan, label, os_id, user_data, backups):
-    url = "https://api.vultr.com/v2/instances"
-    headers = {
-        "Authorization": f"Bearer {api_key}",
-        "Content-Type:": "application/json"
-    }
-    data = {
-        "region": region,
-        "plan": plan,
-        "label": label,
-        "os_id": os_id,
-        "user_data": user_data,
-        "backups": backups
-    }
-    req = urllib.request.Request(url, json.dumps(data).encode(), headers)
-    with urllib.request.urlopen(req) as res:
-        res = res.read()
-        return res
+    try:
+        url = "https://api.vultr.com/v2/instances"
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type:": "application/json"
+        }
+        data = {
+            "region": region,
+            "plan": plan,
+            "label": label,
+            "os_id": os_id,
+            "user_data": user_data,
+            "backups": backups
+        }
+        req = urllib.request.Request(url, json.dumps(data).encode(), headers)
+        with urllib.request.urlopen(req) as res:
+            res = res.read()
+            return res
+    except Exception as e:
+        logger.err(e)
